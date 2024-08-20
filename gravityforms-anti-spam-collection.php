7f9500cfa8e20a1c3f6dcd498811cbb9e619dcbe
Michael <admin@alpha1beta.com>
2024-08-19 22:14:34 -0400

    Update gravityforms-anti-spam-collection.php

--- gravityforms-anti-spam-collection.php
+++ gravityforms-anti-spam-collection.php
@@ -7,9 +7,12 @@ Version: 0.1.0
 Author URI: https://wphacks.org
 */
 
-/*
-lowercase all email addresses
-*/
+function wphacks_anti_spam_collection_log_spam_reason( $entry_obj, $reason, $form ){
+	//$res = wp_cache_set( "entry_". rgar( $entry_obj, "id") ."_spam_reason", $reason, "wphacks_gforms_anti_spam", 300 );
+	GFCommon::set_spam_filter( rgar( $form, 'id' ), "WP Hacks Anti-Spam", $reason );
+}
+
+
 add_filter( 'gform_save_field_value', 'wphacks_gravity_forms_lowercase_all_emails', 10, 5 );
 function wphacks_gravity_forms_lowercase_all_emails( $value, $entry, $field, $form, $input_id ){
 	if($field->type == "email" ){
@@ -38,6 +41,8 @@ function wphacks_mark_as_spam_banned_domains_on_website_fields( $is_spam, $form,
 				print_r( $parsed_url );
 				echo $parsed_url['host'];
 				if(in_array( $parsed_url['host'], apply_filters('wphacks_gravityforms_anti_spam_collections_banned_domains_on_website_fields', $banned_domains ) ) ){
+					$spam_reason = 'Field '. $field->id .' has a banned domain in the website.';
+					wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 					return true;
 				}
 			} 
@@ -74,6 +79,8 @@ function wphacks_mark_as_spam_banned_tlds_on_website_fields( $is_spam, $form, $e
 						}
 						
 						if(  substr( $parsed_url['host'], -$length ) === $banned_tld  ){
+							$spam_reason = 'Field '. $field->id .' has a banned TLD in the website.';
+							wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 							return true;
 						}
 					}
@@ -113,6 +120,8 @@ function wphacks_mark_as_spam_banned_tlds_on_email_fields( $is_spam, $form, $ent
 							}
 							
 							if(  substr( $email_domain, -$length ) === $banned_tld  ){
+								$spam_reason = 'Field '. $field->id .' has a banned TLD in the email address.';
+								wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 								return true;
 							}
 						}
@@ -124,6 +133,44 @@ function wphacks_mark_as_spam_banned_tlds_on_email_fields( $is_spam, $form, $ent
     return $is_spam;
 }
 
+add_filter( 'gform_entry_is_spam', 'wphacks_mark_as_spam_banned_usernames_on_email_fields', 1, 3 );
+function wphacks_mark_as_spam_banned_usernames_on_email_fields( $is_spam, $form, $entry ) {
+	if( $is_spam ){
+		return $is_spam;
+	}
+	
+	$banned_usernames= array(
+		'nobody',
+		'noreply',
+		'no-reply',
+		'postmaster',
+		'abuse'
+	);
+	
+	$banned_usernames = apply_filters('wphacks_gravityforms_anti_spam_collections_banned_usernames_on_email_fields', $banned_usernames );
+	
+	if( isset( $form['fields'] ) ){
+		foreach( $form['fields'] as $field ){
+			if( $field->type == "email"){
+				$email = rgar( $entry, $field->id);
+				if( !empty( $email ) ){
+					$email_explode = explode( '@', $email );
+
+					if( is_array( $email_explode ) ){
+						$email_username = $email_explode[0];
+						if( in_array( strtolower( trim( $email_username ) ), $banned_usernames ) ){
+							
+							$spam_reason = 'Field '. $field->id .' has a banned username in the email address.';
+							wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
+							return true;
+						}
+					}
+				}
+			}
+		}
+	}
+    return $is_spam;
+}
 
 add_filter( 'gform_entry_is_spam', 'wphacks_mark_as_spam_banned_domains_on_email_fields', 10, 3 );
 function wphacks_mark_as_spam_banned_domains_on_email_fields( $is_spam, $form, $entry ) {
@@ -148,6 +195,9 @@ function wphacks_mark_as_spam_banned_domains_on_email_fields( $is_spam, $form, $
 						$email_domain = end( $email_explode );
 						
 						if( in_array( strtolower( trim( $email_domain ) ), $banned_domains ) ){
+							
+							$spam_reason = 'Field '. $field->id .' has a banned domain in the email address.';
+							wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 							return true;
 						}
 					}
@@ -168,7 +218,7 @@ function wphacks_get_ipdata_co_access_token(){
 	return apply_filters('whpacks_get_ipdata_co_access_token', $token);
 }
 
-add_filter( 'gform_entry_is_spam', 'mark_webhosts_as_spam_using_ipdata_co', 10, 3 );
+add_filter( 'gform_entry_is_spam', 'mark_webhosts_as_spam_using_ipdata_co', 1000, 3 );
 function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 	if( $is_spam ){
 		return $is_spam;
@@ -193,10 +243,11 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 	if( !empty( $ip_lookup_results ) ){
 		$ip_lookup_results = json_decode( $ip_lookup_results, true );
 	}
-	
 		//known abusers
 		if( true === apply_filters( 'wphacks_gravityforms_mark_known_abusers_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_known_abuser'] ){
+				$spam_reason = "IP Address ({$ip_address}) is a known abuser.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -204,6 +255,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//known attackers
 		if( true === apply_filters( 'wphacks_gravityforms_mark_known_attackers_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_known_attacker'] ){
+				$spam_reason = "IP Address ({$ip_address}) is a known attacker.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -211,6 +264,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//datacenters
 		if( true === apply_filters( 'wphacks_gravityforms_mark_webhosts_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_datacenter'] ){
+				$spam_reason = "IP Address ({$ip_address}) is datacenter.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -218,6 +273,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//bogons
 		if( true === apply_filters( 'wphacks_gravityforms_mark_bogon_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_bogon'] ){
+				$spam_reason = "IP Address ({$ip_address}) is a bogon.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -225,6 +282,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//threats
 		if( true === apply_filters( 'wphacks_gravityforms_mark_threats_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_threat'] ){
+				$spam_reason = "IP Address ({$ip_address}) is from a known threat.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -232,6 +291,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//is_anonymous
 		if( true === apply_filters( 'wphacks_gravityforms_mark_anonymous_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_anonymous'] ){
+				$spam_reason = "IP Address ({$ip_address}) is an anonymous service.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}	
@@ -239,6 +300,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//is_proxy
 		if( true === apply_filters( 'wphacks_gravityforms_mark_proxy_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_proxy'] ){
+				$spam_reason = "IP Address ({$ip_address}) is a proxy.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -246,6 +309,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//is_proxy
 		if( true === apply_filters( 'wphacks_gravityforms_mark_tor_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_tor'] ){
+				$spam_reason = "IP Address ({$ip_address}) is an Tor exit node.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -253,6 +318,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		//icloud_relays
 		if( true === apply_filters( 'wphacks_gravityforms_mark_icloud_relay_as_spam_using_ipdata_co', true ) ){
 			if( true === $ip_lookup_results['threat']['is_icloud_relay'] ){
+				$spam_reason = "IP Address ({$ip_address}) is an icloud relay.";
+				wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 				return true;
 			}
 		}
@@ -260,6 +327,8 @@ function mark_webhosts_as_spam_using_ipdata_co( $is_spam, $form, $entry ) {
 		if( is_array( $bad_asns) && !empty( $bad_asns ) ){
 			if(isset( $ip_lookup_results['asn']['asn'] ) ){
 				if( in_array( $ip_lookup_results['asn']['asn'], $bad_asns ) ){
+				$spam_reason = "IP Address ({$ip_address}) belongs to a banned ASN: ". $ip_lookup_results['asn']['asn'];
+					wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
 					return true;
 				}
 			}
@@ -295,6 +364,206 @@ function wphacks_unmark_as_spam_allowlist_domains_on_email_fields( $is_spam, $fo
 							return false;
 						}
 					}
+				}
+			}
+		}
+	}
+	return $is_spam;
+}
+
+add_filter( 'gform_entry_is_spam', 'wphacks_mark_as_spam_phony_us_phone_number_fields', 10, 3 );
+function wphacks_mark_as_spam_phony_us_phone_number_fields( $is_spam, $form, $entry ) {
+	if( $is_spam ){
+		return $is_spam;
+	}
+	
+	$bad_phone_numbers = array();
+	
+	$bad_phone_numbers = apply_filters('wphacks_gravityforms_anti_spam_collections_allowlist_domains_on_email_field', $bad_phone_numbers );
+	
+	
+	if( isset( $form['fields'] ) ){
+		foreach( $form['fields'] as $field ){
+			if ( $field->type == 'phone' || ( $field->type == 'text' && $field->inputMask == 1 && $field->inputMaskValue == '(999) 999-9999? x99999') ){
+				
+				$phone = rgar( $entry, $field->id );
+				
+				$phone_number_only = implode( "", array_filter( str_split( $phone ), function( $array ){ return is_numeric( $array ); } ) );
+				
+				if(count( array_unique( array_filter( str_split( $phone_number_only ),function( $array ){ return is_numeric( $array ); } ) ) ) === 1){
+					$spam_reason = 'Field '. $field->id .' uses a phone number consisting entirely of the same digit.';
+					wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
+					return true;
+				}
+				
+				if( substr( $phone_number_only, 3, 7) == 8675309 ){
+					$spam_reason = 'Unless the form was submitted by Jenny, Field '. $field->id .' has a fake phone number.';
+					wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
+					return true;
+				}
+				
+
+				//for 10 digit phone numbers
+				echo $phone_number_only;
+				if( strlen( $phone_number_only ) === 10 ) {
+					echo substr( $phone_number_only, 7, 3 );
+					if( in_array( substr( $phone_number_only, 3, 4 ), array( 5550 )  ) && in_array( substr( $phone_number_only, 7, 3 ), range('100','199') ) ){
+						$spam_reason = 'Field '. $field->id .' uses an fictional number.';
+						wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
+						return true;
+					}
+					
+					$unassigned_us_area_codes = array(
+						221,
+						230,
+						232,
+						237,
+						238,
+						241,
+						243,
+						247,
+						258,
+						259,
+						261,
+						265,
+						271,
+						273,
+						275,
+						278,
+						280,
+						282,
+						285,
+						286,
+						287,
+						328,
+						335,
+						338,
+						342,
+						348,
+						349,
+						356,
+						358,
+						359,
+						381,
+						383,
+						384,
+						389,
+						420,
+						421,
+						426,
+						427,
+						429,
+						439,
+						446,
+						449,
+						451,
+						452,
+						453,
+						454,
+						456,
+						459,
+						461,
+						462,
+						465,
+						467,
+						471,
+						476,
+						481,
+						482,
+						485,
+						486,
+						487,
+						489,
+						536,
+						560,
+						565,
+						575,
+						583,
+						625,
+						627,
+						632,
+						634,
+						635,
+						637,
+						642,
+						643,
+						648,
+						652,
+						653,
+						654,
+						663,
+						665,
+						668,
+						673,
+						674,
+						675,
+						676,
+						685,
+						687,
+						723,
+						735,
+						736,
+						739,
+						741,
+						745,
+						746,
+						749,
+						750,
+						751,
+						752,
+						756,
+						759,
+						761,
+						764,
+						768,
+						783,
+						789,
+						823,
+						824,
+						827,
+						834,
+						836,
+						841,
+						842,
+						846,
+						852,
+						853,
+						871,
+						874,
+						875,
+						921,
+						923,
+						926,
+						927,
+						932,
+						935,
+						953,
+						957,
+						958,
+						974,
+						976,
+						981,
+						982,
+						987
+					);
+					if( in_array(substr( $phone_number_only, 0, 3), $unassigned_us_area_codes ) ){
+						$spam_reason = 'Field '. $field->id .' uses an unassigned area code.';
+						wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
+						return true;
+					}
+					
+					$bad_area_codes = array_merge(range(000,199), range(990,999) );
+					$bad_area_codes = array_map( function( $item ){ return str_pad( $item, 3,0, STR_PAD_LEFT ); }, $bad_area_codes);
+					
+					
+					if( in_array(substr( $phone_number_only, 0, 3), $bad_area_codes ) ){
+						
+						$spam_reason = 'Field '. $field->id .' has a bad area code.';
+						wphacks_anti_spam_collection_log_spam_reason( $entry, $spam_reason, $form );
+						return true;
+					}
+				} else {
+					
 				}
 			}
 		}
